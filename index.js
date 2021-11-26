1d218c6dd22673f220bfaece4ad3854fff22b853
Andrew Lita <lita.study@bk.ru>
2021-11-26 08:56:14 +0300

    refactor: POST method finished

--- index.js
+++ index.js
@@ -9,27 +9,30 @@ if (envParsed.error) {
 }
 const host = envParsed.HOST || 'localhost';
 const port = envParsed.PORT || 3000;
+exports.global = staff = [];
 
 const server = http.createServer();
 server.listen(port, () => console.log('server is running on port', port));
 server.on('request', reqHandler);
 
-
 function reqHandler(request, response) {
     // response.end("response = OK");
     let { url, method, headers } = request;
     const [path, id] = url.split("/").splice(1);
-    body = [];
+    reqBody = [];
     request.on('error', (err) => console.log(err));
-    request.on('data', (chunk) => body.push(chunk));
+    request.on('data', (chunk) => reqBody.push(chunk));
     request.on('end', () => {
-        body = Buffer.concat(body).toString();
-        let { operStatus, operMessage } = router(response, method, path, id);
+        reqBody = Buffer.concat(reqBody).toString();
+        let { resStatus, resMessage, resBody } = router(reqBody, method, path, id);
         response.on('error', (err) => {
             console.error(err);
         });
-        response.writeHead(operStatus, operMessage, { 'Content-Type': 'application/json' })
+        response.writeHead(resStatus, resMessage, { 'Content-Type': 'application/json' })
+        response.write(resBody);
         response.end();
     });
+
+    console.log("staff=", staff);
 }
 
